1472710654ff368512bdc3d7dff08893b88b450e
Orkhan <orkhandg@code.edu.az>
2019-09-28 11:18:09 +0400

    active nav link done

--- js/include.js
+++ js/include.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
-    $(".header").load("header.html");
+    $(".header").load("header.html", setActiveMenu);
     $(".wine-foot").load("footer.html");
 });
+
+function setActiveMenu() {
+    let path = window.location.pathname;
+    if (window.location.pathname.startsWith('/'))
+        path = path.substr(1);
+    $('#inv-menu > li').removeClass('active');
+    $(`a[href='${path}']`).closest('li').addClass('active');
+}
